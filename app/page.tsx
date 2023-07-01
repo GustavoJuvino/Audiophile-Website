4bb5287e044038faa657eb8ff410af6fdb8e8284
GustavoSouza <juvinogustavo1@gmail.com>
2023-07-01 16:25:19 -0300

    building home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,26 @@ import Button from "./Components/Button";
 export default function Home() {
   return (
     <main className="bg-onyx">
-      <section className="w-[1110px] h-[729px] flex">
+      <section className="
+          w-[1110px]
+          h-[729px]
+          flex
+          justify-between
+          max-xl:sm:ml-8
+          max-xl:overflow-hidden
+        "
+      >
 
         {/* Texts */}
-        <div className="text-white">
+        <div className="text-white flex flex-col justify-center">
           <span className="text-overline uppercase opacity-50">
             new product
           </span>
-          <h1 className="text-6xl">
+          <h1 className="text-6xl my-6">
             XX99 Mark II <br />
             Headphones
           </h1>
-          <p className="text-base opacity-75">
+          <p className="w-[85%] text-base opacity-75 mb-10">
             Experience natural, lifelike audio and exceptional
             build quality made for the passionate music
             enthusiast.
@@ -30,7 +38,7 @@ export default function Home() {
             height={886}
             alt="Headphones-Image"
             src="/assets/home/desktop/image-main.png"
-            className="absolute w-auto h-[886px] object-cover z-[2]"
+            className="absolute bottom-[-6.5rem] w-auto h-[886px] object-cover z-[2]"
           />
           <div className="
               w-[500px]
@@ -38,10 +46,11 @@ export default function Home() {
               bg-[#2B2B2B]
               rounded-full
               absolute
-              bottom-[6.5rem]
+              bottom-[11rem]
               left-[1rem]
               z-[1]
               blur-3xl
+              max-lg:hidden
             "
           />
         </div>
