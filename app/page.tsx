8a72ba7d3d48f5ba019f003b9d4794d60e3f2d0d
GustavoSouza <juvinogustavo1@gmail.com>
2023-07-01 11:07:13 -0300

    Fixing main image

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Button from "./Components/Button";
 export default function Home() {
   return (
     <main className="bg-onyx">
-      <section className="w-[1110px] h-[729px]">
+      <section className="w-[1110px] h-[729px] flex">
 
         {/* Texts */}
         <div className="text-white">
@@ -24,13 +24,27 @@ export default function Home() {
         </div>
 
         {/* Image */}
-        <Image 
-          width={700}
-          height={886}
-          alt="Headphones-Image"
-          src="/assets/home/desktop/image-hero.jpg"
-          className="absolute w-auto h-[520px] object-cover "
-        />
+        <div className="w-[700px] h-auto relative">
+          <Image 
+            width={700}
+            height={886}
+            alt="Headphones-Image"
+            src="/assets/home/desktop/image-main.png"
+            className="absolute w-auto h-[886px] object-cover z-[2]"
+          />
+          <div className="
+              w-[500px]
+              h-[500px]
+              bg-[#2B2B2B]
+              rounded-full
+              absolute
+              bottom-[6.5rem]
+              left-[1rem]
+              z-[1]
+              blur-3xl
+            "
+          />
+        </div>
       </section>
     </main>
   )
