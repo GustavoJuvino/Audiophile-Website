92f349de8898779a3c27f2de7575d49f9a622043
GustavoSouza <juvinogustavo1@gmail.com>
2023-07-21 10:51:09 -0300

    style: add styles in the form within Checkout component

---
+++ app/Components/Checkout.tsx
@@ -0,0 +1,214 @@
+"use client";
+
+import React, { useState } from 'react';
+import { Form } from "./Form";
+
+const Checkout = () => {
+    const [selectedRadioBtn, setSelectedRadionBtn] = useState("e-money");
+
+    const isRadioSelected = (value: string): boolean => selectedRadioBtn === value;
+    const handleRadioCheck = (e: React.ChangeEvent<HTMLInputElement>): void => setSelectedRadionBtn(e.currentTarget.value);
+
+    return (
+        <form className="w-[730px] h-full bg-white flex flex-col px-12">
+            <h1 className="mt-[54px] text-4xl uppercase">
+                Checkout
+            </h1>
+
+            <section className="mt-10">
+                <legend className="
+                            font-bold
+                            mb-4
+                            uppercase
+                            tracking-[0.93px]
+                            text-raw-sienna
+                        "
+                >
+                    Billing Details
+                </legend>
+
+                <div className="flex gap-x-4 gap-y-6 flex-wrap">
+                    <Form.Field>
+                        <Form.Label htmlFor="name">
+                            Name
+                        </Form.Label>
+
+                        <Form.Input
+                            type="string"
+                            name="name"
+                            placeholder="Alexei Ward"
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <Form.Label htmlFor="email">
+                            Email Adress
+                        </Form.Label>
+
+                        <Form.Input
+                            type="email"
+                            name="email"
+                            placeholder="alexei@mail.com"
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <Form.Label htmlFor="phone-number">
+                            Phone Number
+                        </Form.Label>
+
+                        <Form.Input
+                            type="number"
+                            name="phone-number"
+                            placeholder="+1 202-555-0136"
+                        />
+                    </Form.Field>
+                </div>
+            </section>
+
+            <section className="mt-[54px]">
+                <legend className="
+                            font-bold
+                            mb-4
+                            uppercase
+                            tracking-[0.93px]
+                            text-raw-sienna
+                        "
+                >
+                    Shipping Info
+                </legend>
+
+                <div className="flex flex-wrap gap-x-4 gap-y-6">
+                    <Form.Field>
+                        <Form.Label htmlFor="phone-number">
+                            Address
+                        </Form.Label>
+
+                        <Form.Input
+                            type="string"
+                            name="address"
+                            placeholder="1137 Williams Avenue"
+                            className="w-[634px]"
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <Form.Label htmlFor="zip-code">
+                            ZIP Code
+                        </Form.Label>
+
+                        <Form.Input
+                            type="number"
+                            name="zip-code"
+                            placeholder="10001"
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <Form.Label htmlFor="city">
+                            City
+                        </Form.Label>
+
+                        <Form.Input
+                            type="string"
+                            name="city"
+                            placeholder="New York"
+                        />
+                    </Form.Field>
+
+                    <Form.Field>
+                        <Form.Label htmlFor="country">
+                            Country
+                        </Form.Label>
+
+                        <Form.Input
+                            type="string"
+                            name="country"
+                            placeholder="United States"
+                        />
+                    </Form.Field>
+                </div>
+            </section>
+
+            <section className="mt-[60px]">
+                <legend className="
+                            font-bold
+                            mb-4
+                            uppercase
+                            tracking-[0.93px]
+                            text-raw-sienna
+                        "
+                >
+                    Payment Details
+                </legend>
+
+                <div className="w-full flex justify-between">
+                    <h2>
+                        Payment Method
+                    </h2>
+
+                    <div className="flex flex-col gap-4">
+                        <Form.Field className={`
+                                flex
+                                items-center
+                                pl-4
+                                pt-[14px]
+                                gap-4
+                                w-[309px]
+                                h-14
+                                border-[1px]
+                                rounded-lg
+                                duration-300
+                                ${selectedRadioBtn === "e-money" && "border-raw-sienna"}
+                            `}
+                        >
+                            <Form.Input
+                                type="radio"
+                                name="e-money"
+                                value="e-money"
+                                checked={isRadioSelected("e-money")}
+                                onChange={handleRadioCheck}
+                                className={selectedRadioBtn === "e-money" ? "selected" : ""}
+                            />
+
+                            <Form.Label htmlFor="e-money">
+                                e-Money
+                            </Form.Label>
+                        </Form.Field>
+
+                        <Form.Field className={`
+                            flex
+                            items-center
+                            pl-4
+                            pt-[14px]
+                            gap-4
+                            w-[309px]
+                            h-14
+                            border-[1px]
+                            rounded-lg
+                            duration-300
+                            ${selectedRadioBtn === "cash" && "border-raw-sienna"}
+                        `}
+                                
+                                
+                        >
+                            <Form.Input
+                                type="radio"
+                                name="cash"
+                                value="cash"
+                                checked={isRadioSelected("cash")}
+                                onChange={handleRadioCheck}
+                                className={selectedRadioBtn === "cash" ? "selected" : ""}
+                            />
+                            <Form.Label htmlFor="cash">
+                                Cash on Delivery
+                            </Form.Label>
+                        </Form.Field>
+                    </div>
+                </div>
+            </section>
+        </form>
+    )
+}
+
+export default Checkout;
--- app/Components/Form/Input.tsx
+++ app/Components/Form/Input.tsx
@@ -1,25 +1,40 @@
-import { InputHTMLAttributes } from "react";
+"use client";
+import { InputHTMLAttributes, useEffect, useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import useClickOutside from "@/app/hooks/useClickOutside";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
 }
 
-export function Input(props: InputProps) {
+export function Input({ name, ...props}: InputProps) {
+    const [selected, setSelected] = useState(false);
+    const inputRef = useRef(null);
+    const { clickOutside } = useClickOutside();
+
+    useEffect(() => {
+        if(inputRef) clickOutside(inputRef, setSelected)
+    }, [])
+
     return (
-        <input 
-            id={props.name}
-            className="
-                w-[310px]
-                h-14
-                border-[1px]
-                pl-6
-                border-[#CFCFCF]
-                rounded-lg
-                outline-none
-                text-black
-                font-bold
-            "
+        <input
             {...props}
+            id={name}
+            ref={inputRef}
+            onClick={() => setSelected(true)}
+            className={twMerge(`
+                    w-[309px]
+                    h-14
+                    border-[1px]
+                    pl-6
+                    rounded-lg
+                    outline-none
+                    text-black
+                    font-bold
+                    duration-300
+                    ${selected ? "border-raw-sienna" : "border-[#CFCFCF]"}
+                `, props.className)
+            }
         />
     )
 }
